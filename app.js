e6f0477a4c2527dee341d8ea2ddb7c24e2a9f8e6
EcaCosca <enriquecoscarelli@gmail.com>
2023-04-13 03:47:58 -0600

    Stage 3

--- app.js
+++ app.js
@@ -4,11 +4,44 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000
 
-app.get('/', (req, res)=> res.send('GET: Reading something!'));
-app.post('/', (req, res)=> res.send('POST: Creating something!'));
-app.put('/', (req, res)=> res.send('PUT: Editing something!'));
-app.delete('/', (req, res)=> res.send('DELETE: Deleting something!'));
+// STAGE 1
+// app.get('/', (req, res)=> res.send('GET: Reading something!'));
+// app.post('/', (req, res)=> res.send('POST: Creating something!'));
+// app.put('/', (req, res)=> res.send('PUT: Editing something!'));
+// app.delete('/', (req, res)=> res.send('DELETE: Deleting something!'));
 
+// STAGE 2 - ROUTING
+app.route('/') 
+    .get((req, res)=> res.send('GET: Reading something!'))
+    .post((req, res)=> res.send('POST: Creating something!'))
+    .put((req, res)=> res.send('PUT: Editing something!'))
+    .delete((req, res)=> res.send('DELETE: Deleting something!'));
+    
 app.get('/here', (req, res)=> res.send("The secret is: "+ process.env.S3_BUCKET));
+
+// JSON DATA EXAMPLE
+app.get('/data', (req, res)=>{
+    const data = {
+        name: "Paco",
+        age: 22,
+        email: "paco@tio.com"
+    }
+
+    res.json(data)
+})
+
+// REDIRECT
+app.get('/redirect', (req, res)=>{
+    console.log("you are being redirected")
+    res.redirect('/data')
+})
+
+// Get elements from URL
+
+app.get('/users/:id', (req, res)=>{
+    console.log(req.params.id)
+    console.log(req.query.sort)
+    res.send("Check your console")
+})
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}/`))
